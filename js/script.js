d0868f8ef2ccc39dac8c1972523e9b6a7d7ae5ec
Billal Asif <asif.billal@outlook.com>
2021-10-19 18:39:29 +0100

    JS continue

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 //Timer
-const startButton = document.getElementById("startButton").addEventListener('click', startTimer());
-const stopButton = document.getElementById("stopButton").addEventListener('click', stopTimer());
+
+const startButton = document.getElementById("startButton").addEventListener('click', () => { });
+const pauseButton = document.getElementById("pauseButton").addEventListener('click', () => { });
+const resetButton = document.getElementById("resetButton").addEventListener('click', () => { });
+const increaseCount = document.getElementById("increaseCount").addEventListener('click', () => {
+    updateCounter(15);
+});
+
+updateCounter = (count) => {
+    let currentCount;
+    currentCount += count;
+    const counter = document.getElementById("counter").innerHTML = currentCount;
+}
