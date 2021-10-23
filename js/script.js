0afcd4805a913f44e11197cb5acbb3249e8c4bed
Billal Asif <asif.billal@gmail.com>
2021-10-24 00:14:38 +0100

    Timer JS update

--- js/script.js
+++ js/script.js
@@ -1,34 +1,39 @@
 //Timer
 let isPaused = false;
 let counterDisplay = document.getElementById("counter");
-let currentSecs = 00;
-let currentMins = 00;
-let currentHours = 00;
-displayCounter(0);
-
+var timer = new Date("Mar 15, 2019 00:00:00").getTime();
+displayTimer(0,0,0);
+timer = 0 /timer;
 const pauseButton = document.getElementById("pauseButton").addEventListener('click', () => { });
 const resetButton = document.getElementById("resetButton").addEventListener('click', () => {
-    updateCounter(0);
+    displayTimer(0,0,0);
+    timer = 0 /timer;
 });
 const increaseCountButton = document.getElementById("increaseCount").addEventListener('click', () => {
-    updateCounter(15);
+    updateTimer(900000);
 });
 const decreaseCountButton = document.getElementById("decreaseCount").addEventListener('click', () => {
-    updateCounter(-15);
+    updateTimer(-900000);
 });
-
-const updateCounter = (count) => {
-    if (currentSecs === 0 && currentMins === 0 && currentHours === 0 && count < 0) {
-        alert('Can not decrease timer below zreo!');
-    }
-    else {
-        displayCounter(count);
-    }
-}
 
 const startButton = document.getElementById("startButton").addEventListener('click', () => {
     loop();
 });
+
+function updateTimer(time) {
+timer = timer + time;
+let hours;
+let mins;
+let secs;
+    if (timer > 0){
+        hours = Math.floor((timer % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        mins = Math.floor((timer % (1000 * 60 * 60)) / (1000 * 60));
+        secs = Math.floor((timer % (1000 * 60)) / 1000);
+        displayTimer(hours, mins, secs)
+    } else {
+
+    }
+}
 
 function loop() {
     if (currentCount > 0) {
@@ -44,30 +49,13 @@ function startCounter() {
     }, 1000)
 }
 
-function displayCounter(mins) {
-    if (mins < 0 && currentMins == 0 && currentHours > 0) {
-        currentHours = currentHours - 1;
-        currentMins = 45;
+function displayTimer(hours, mins, secs) {
+    if (hours == 0 && mins == 0 && secs == 0) {
+        counterDisplay.innerHTML = hours + ":" + mins + ":" + secs;
+    }else if (hours == 0 && mins < 0) {
+        alert("Timer can not go below Zero!");
     }
-    else if (currentMins >= 45){
-        currentHours = currentHours + 1;
-        currentMins = 00;
-        currentSecs = 00;
-        addCounterToDom();
-    } else {
-        currentSecs = 00;
-        currentMins += mins;
-        currentHours = 00;
-        addCounterToDom();
+    else {
+        counterDisplay.innerHTML = hours + ":" + mins + ":" + secs;
     }
 }
-
-function addCounterToDom() {
-    counterDisplay.innerHTML = currentHours + ":" + currentMins + ":" + currentSecs;
-}
-
-let date = new Date;
-alert(date.getDate(0));
-
-//check it out
-//https://www.developerdrive.com/build-countdown-timer-pure-javascript/#:~:text=%20How%20to%20Build%20a%20Countdown%20Timer%20in,two%20new%20local%20variables.%20The%20first...%20More%20
