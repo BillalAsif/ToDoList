0db099d5a214b9a918589d975309bf5ee36259db
Billal Asif <asif.billal@outlook.com>
2021-10-25 22:34:46 +0100

    timer in js updated

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ const startButton = document.getElementById("startButton");
 const pauseButton = document.getElementById("pauseButton");
 
 //Dispaly timer as zero onload
-displayTimer(0,0,0);
-timer = 0 /timer;
+displayTimer(0, 0, 0);
+timer = 0 / timer;
 
 //Disbale start/pause button onload
 startButton.classList.add("disabled");
@@ -18,14 +18,14 @@ pauseButton.classList.add("disabled");
 
 //Buuton triggers
 resetButton.addEventListener('click', () => {
-    displayTimer(0,0,0);
-    timer = 0 /timer;
-    startButton.classList.add("disabled");
-    pauseButton.classList.add("disabled");
+    displayTimer(0, 0, 0);
+    timer = 0 / timer;
+    timerRunState('reset');
 });
 
 increaseCountButton.addEventListener('click', () => {
     updateTimer(900000);
+    startButton.classList.remove('disabled');
 });
 
 decreaseCountButton.addEventListener('click', () => {
@@ -33,42 +33,58 @@ decreaseCountButton.addEventListener('click', () => {
         alert("Timer can not go below Zero!");
     } else {
         updateTimer(-900000);
+        startButton.classList.remove('disabled');
     }
 });
 
 startButton.addEventListener('click', () => {
     var startCounter = setInterval(() => {
         updateTimer(-1000)
-        startButton.classList.add('disabled');
-        if(isPaused == true){
+        if (isPaused == true) {
             clearInterval(startCounter);
         }
     }, 1000);
     startCounter;
-    pauseButton.classList.toggle('disabled');
+    timerRunState('start');
 });
 
 pauseButton.addEventListener('click', () => {
     isPaused = true;
-    startButton.classList.toggle('disabled');
-    pauseButton.classList.toggle('disabled');
- });
+    timerRunState('pause');
+});
 
- //Functions
+//Functions
 function updateTimer(time) {
-timer = timer + time;
-let hours;
-let mins;
-let secs;
-    if (timer > 0){
+    timer = timer + time;
+    let hours;
+    let mins;
+    let secs;
+    if (timer > 0) {
         hours = Math.floor((timer % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         mins = Math.floor((timer % (1000 * 60 * 60)) / (1000 * 60));
         secs = Math.floor((timer % (1000 * 60)) / 1000);
         displayTimer(hours, mins, secs)
-        startButton.classList.toggle('disabled');
     }
 }
 
 function displayTimer(hours, mins, secs) {
-        counterDisplay.innerHTML = hours + ":" + mins + ":" + secs;
+    counterDisplay.innerHTML = hours + ":" + mins + ":" + secs;
+}
+
+function timerRunState(state) {
+    switch (state) {
+        case 'start':
+            startButton.classList.remove('disabled');
+            pauseButton.classList.remove('disabled');
+            startButton.classList.add('disabled');
+            break;
+        case 'pause':
+            startButton.classList.remove('disabled');
+            pauseButton.classList.remove('disabled');
+            pauseButton.classList.add('disabled');
+            break;
+        case 'reset':
+            pauseButton.classList.add('disabled');
+            startButton.classList.add('disabled');
+    }
 }
